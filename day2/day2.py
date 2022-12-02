1ebf892ea50323983ceafd5aa4ec5b68450da64e
Malcolm Peterson <mgpetersn@gmail.com>
2022-12-02 14:58:12 -0500

    first pass at day2

---
+++ day2/day2.py
@@ -0,0 +1,72 @@
+# What would your total score be if everything goes exactly according to your strategy guide?
+# points = rock : 1 \paper : 2 \scissors: 3
+#          lose: 0 \draw : 3 \win : 6
+
+throwPoints = {
+    "X": 1,  # rock
+    "A": 1,
+    "Y": 2,  # paper
+    "B": 2,
+    "Z": 3,  # scissors
+    "C": 3
+}
+
+with open('day2/input.txt') as input:
+    scoreTally = 0
+
+    lines = input.readlines()
+    for line in lines:
+        round = line.split(' ')
+        # scoreTally += throwPoints[line[2]]
+        # if throwPoints[line[2]] ==1:
+        #     if throwPoints[line[0]] == 1:
+        #         scoreTally+=3
+        #     elif throwPoints[line[0]] == 2:
+        #         scoreTally+=0
+        #     if throwPoints[line[0]] ==3:
+        #         scoreTally+=6
+        # if throwPoints[line[2]] == 2:
+        #     if throwPoints[line[0]] == 1:
+        #         scoreTally+=6
+        #     elif throwPoints[line[0]] == 2:
+        #         scoreTally+=3
+        #     if throwPoints[line[0]] ==3:
+        #         scoreTally+=0
+        # if throwPoints[line[2]] == 3:
+        #     if throwPoints[line[0]] == 1:
+        #         scoreTally+=0
+        #     elif throwPoints[line[0]] == 2:
+        #         scoreTally+=6
+        #     if throwPoints[line[0]] ==3:
+        #         scoreTally+=3
+
+        if throwPoints[line[2]] == 1:
+            scoreTally+=0
+        elif throwPoints[line[2]] == 2:
+            scoreTally+=3
+        elif throwPoints[line[2]] == 3:
+            scoreTally+=6
+
+        if throwPoints[line[0]] == 1:
+            if throwPoints[line[2]] == 1:
+                scoreTally += 3 #scissors
+            elif throwPoints[line[2]] == 2:
+                scoreTally += 1 #rock
+            elif throwPoints[line[2]] == 3:
+                scoreTally += 2 #paper
+        elif throwPoints[line[0]] == 2:
+            if throwPoints[line[2]] == 1:
+                 scoreTally += 1 #rock
+            elif throwPoints[line[2]] == 2:
+                 scoreTally += 2 #paper
+            elif throwPoints[line[2]] == 3:
+                scoreTally += 3 #scissors
+        elif throwPoints[line[0]] == 3:
+            if throwPoints[line[2]] == 1:
+                scoreTally += 2 #paper    
+            elif throwPoints[line[2]] == 2:
+                scoreTally += 3 #scissors
+            elif throwPoints[line[2]] == 3:        
+                scoreTally += 1 #rock
+
+    print(scoreTally)
